cbc74b0017df58670e6d838844734e0b3ca72bdf
walkinglele <leleberjalan@gmail.com>
2021-03-15 10:30:27 +0700

    Initial Commit

---
+++ Youtube Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Youtube Clone
+//
+//  Created by Dzaky Saputra on 15/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
